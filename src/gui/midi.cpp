efef2958187408aa563eb38ef7701cc01d25f5a1
morphinejh <morphinejh@hotmail.com>
2022-06-10 22:46:39 -0400

    DosboxECE r4479 updates

--- src/gui/midi.cpp
+++ src/gui/midi.cpp
@@ -71,6 +71,13 @@ MidiHandler Midi_none;
 /* Include different midi drivers, lowest ones get checked first for default.
    Each header provides an independent midi interface. */
 
+#ifdef C_FLUIDSYNTH
+#include "midi_fluidsynth.h"
+#endif
+
+#include "midi_mt32.h"
+static MidiHandler_mt32 &Midi_mt32 = MidiHandler_mt32::GetInstance();
+
 #if defined(MACOSX)
 
 #if defined(C_SUPPORTS_COREMIDI)
@@ -96,13 +103,6 @@ MidiHandler Midi_none;
 #include "midi_alsa.h"
 
 #endif
-
-#ifdef C_FLUIDSYNTH
-#include "midi_fluidsynth.h"
-#endif
-
-#include "midi_mt32.h"
-static MidiHandler_mt32 &Midi_mt32 = MidiHandler_mt32::GetInstance();
 
 DB_Midi midi;
 
